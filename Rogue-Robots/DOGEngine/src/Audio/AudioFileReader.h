7cef472b3bf21990f630a0907638f0f43a2a604d
Joyoslive <joyoslive@gmail.com>
2022-09-07 09:30:30 +0200

    Change WAV data to use vector instead of unique ptr

--- Rogue-Robots/DOGEngine/src/Audio/AudioFileReader.h
+++ Rogue-Robots/DOGEngine/src/Audio/AudioFileReader.h
@@ -22,30 +22,30 @@ namespace DOG
 		u16 bps;
 	};
 
-	static WAVProperties ReadWAVProperties(const u8* data, u64 dataSize)
+	static WAVProperties ReadWAVProperties(const std::vector<u8>& data)
 	{
-		if (dataSize < WAV_HEADER_LENGTH)
+		if (data.size() < WAV_HEADER_LENGTH)
 		{
 			throw std::runtime_error("WAV File is too small to contain a header");
 		}
-		if (memcmp(data, "RIFF", 4) != 0 || memcmp(&data[8], "WAVE", 4) != 0)
+		if (memcmp(data.data(), "RIFF", 4) != 0 || memcmp(&data[8], "WAVE", 4) != 0)
 		{
 			throw std::runtime_error("Provided file is not a WAV file");
 		}
 
 
 		WAVProperties out = {
-			.format = *reinterpret_cast<const WAVFormat*>(&data[8]),
-			.channels = *reinterpret_cast<const u16*>(&data[22]),
-			.sampleRate = *reinterpret_cast<const u32*>(&data[24]),
-			.bps = *reinterpret_cast<const u16*>(&data[34]),
+			.format = *(WAVFormat*)(&data[8]),
+			.channels = *(u16*)(&data[22]),
+			.sampleRate = *(u32*)(&data[24]),
+			.bps = *(u16*)(&data[34]),
 		};
 		return out;
 	}
 
 	//Reads the provided WAV file and returns a tuple of it's properties, a pointer to the sound data, and
 	// the size of said data.
-	static std::tuple<WAVProperties, std::unique_ptr<u8>, u64> ReadWAV(const char* filePath)
+	static std::pair<WAVProperties, std::vector<u8>> ReadWAV(const std::string& filePath)
 	{
 		std::ifstream file(filePath, std::ios::binary);
 		if (!file)
@@ -57,15 +57,15 @@ namespace DOG
 		const u64 fileSize = std::filesystem::file_size(filePath);
 		const size_t dataSize = fileSize - WAV_HEADER_LENGTH;
 
-		std::unique_ptr<u8> wavHeader(new u8[WAV_HEADER_LENGTH]);
-		std::unique_ptr<u8> data(new u8[dataSize]);
+		std::vector<u8> wavHeader(WAV_HEADER_LENGTH);
+		std::vector<u8> data(dataSize);
 
-		file.read(reinterpret_cast<char*>(wavHeader.get()), WAV_HEADER_LENGTH);
-		file.read(reinterpret_cast<char*>(data.get()), dataSize);
+		file.read((char*)(wavHeader.data()), WAV_HEADER_LENGTH);
+		file.read((char*)(data.data()), dataSize);
 
-		WAVProperties properties = ReadWAVProperties(wavHeader.get(), WAV_HEADER_LENGTH);
+		WAVProperties properties = ReadWAVProperties(wavHeader);
 
-		return {properties, std::move(data), dataSize};
+		return {properties, data};
 	}
 }
 
