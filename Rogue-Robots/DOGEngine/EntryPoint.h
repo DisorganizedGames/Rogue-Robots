c77cb4d25099afd4dedd2eee1f16bab8025fdc80
SamilamX <30898665+SamilamX@users.noreply.github.com>
2022-09-14 10:42:05 +0200

    Removed test code from entrypoint.h

--- Rogue-Robots/DOGEngine/EntryPoint.h
+++ Rogue-Robots/DOGEngine/EntryPoint.h
@@ -1,94 +1,25 @@
-//#pragma once
-//#include "src/Core/Application.h"
-//	int main(int, char**)
-//	{
-//
-//		HR hr = CoInitialize(nullptr);
-//		hr.try_fail("Failed to initialize COM");
-//
-//		std::unique_ptr<DOG::Application> app{ nullptr };
-//		while (DOG::ApplicationManager::ShouldRestartApplication())
-//		{
-//			if (!DOG::ApplicationManager::ShouldRestartApplication())
-//				app = std::move(CreateApplication());
-//			else
-//				app->OnRestart();
-//
-//			app->Run();
-//		}
-//
-//		CoUninitialize();
-//		
-//		return 0;
-//	}
-//
-//	extern std::unique_ptr<DOG::Application> CreateApplication() noexcept;
 #pragma once
 #include "src/Core/Application.h"
-#include "src/Network/Server.h"
-#include "src/Network/Client.h"
+	int main(int, char**)
+	{
 
+		HR hr = CoInitialize(nullptr);
+		hr.try_fail("Failed to initialize COM");
 
-int main(int, char**)
-{
-	DOG::Server serverTest;
-	DOG::Client clientTest;
-	DOG::Client::ClientsData testInput;
-	DOG::Client::ClientsData* testOutput;
-	if(getchar() == 's')
-		serverTest.StartTcpServer();
-	testInput.player_nr = clientTest.ConnectTcpServer("192.168.1.55");
-
-	//HR hr = CoInitialize(nullptr);
-	//hr.try_fail("Failed to initialize COM");
-
-	//std::unique_ptr<DOG::Application> app{ nullptr };
-	//while (DOG::ApplicationManager::ShouldRestartApplication())
-	//{
-	//	if (!DOG::ApplicationManager::ShouldRestartApplication())
-	//		app = std::move(CreateApplication());
-	//	else
-	//		app->OnRestart();
-
-	//	app->Run();
-	//}
-
-	//CoUninitialize();
-	while (true) {
-		std::string inputs = "";
-		if (GetAsyncKeyState(87))
+		std::unique_ptr<DOG::Application> app{ nullptr };
+		while (DOG::ApplicationManager::ShouldRestartApplication())
 		{
-			inputs.push_back('w');
+			if (!DOG::ApplicationManager::ShouldRestartApplication())
+				app = std::move(CreateApplication());
+			else
+				app->OnRestart();
 
-		}
-		if (GetAsyncKeyState(68))
-		{
-			inputs.push_back('d');
-
-		}
-		if (GetAsyncKeyState(83))
-		{
-			inputs.push_back('s');
-
-		}
-		if (GetAsyncKeyState(65))
-		{
-			inputs.push_back('a');
-
-		}
-		testInput = clientTest.AddString(testInput, inputs);
-		testOutput = clientTest.SendandReciveTcp(testInput);
-		for (int i = 0; i < 4; i++)
-		{
-			for(int j = 0; j < testOutput[i].inputLength; j++)
-				std::cout << "From player " << testOutput[i].player_nr << ": " <<
-					testOutput[i].inputs[0] << testOutput[i].inputs[1] << std::endl;
+			app->Run();
 		}
 
-		testInput = clientTest.CleanClientsData(testInput);
-		continue;
+		CoUninitialize();
+		
+		return 0;
 	}
-	return 0;
-}
 
-extern std::unique_ptr<DOG::Application> CreateApplication() noexcept;
+	extern std::unique_ptr<DOG::Application> CreateApplication() noexcept;
