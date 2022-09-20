0f49f52d3a407042670208036d285aaa9ebc8e9e
EmilFransson <emil.fransson.1@hotmail.com>
2022-09-20 14:31:23 +0200

    Minor edit

--- Rogue-Robots/DOGEngine/src/ECS/Component.h
+++ Rogue-Robots/DOGEngine/src/ECS/Component.h
@@ -4,7 +4,7 @@ namespace DOG
 	struct ComponentBase
 	{
 		ComponentBase() noexcept = default;
-		virtual ~ComponentBase() noexcept = default;
+	private:
 		[[nodiscard]] static const u32 GetID() noexcept;
 	};
 
@@ -12,7 +12,6 @@ namespace DOG
 	struct Component : public ComponentBase
 	{
 		Component() noexcept = default;
-		virtual ~Component() noexcept override = default;
 		static u32 ID;
 	};
 
@@ -22,14 +21,12 @@ namespace DOG
 	struct TransformComponent : public Component<TransformComponent>
 	{
 		TransformComponent(Vector3f position = { 0.0f, 0.0f, 0.0f }) noexcept : m_position{ position } {}
-		virtual ~TransformComponent() noexcept override final = default;
 		Vector3f m_position;
 	};
 
 	struct ModelComponent : public Component<ModelComponent>
 	{
 		ModelComponent(u64 id = 0) noexcept : id{ id } {}
-		virtual ~ModelComponent() noexcept override final = default;
 		u64 id;
 	};
 }
