d73dedc1c3fe33cb1484f9589fed4680414d7d28
Joyoslive <joyoslive@gmail.com>
2022-09-22 11:19:46 +0200

    Update main example

--- Offline-Tools/AssetManager/src/main.cpp
+++ Offline-Tools/AssetManager/src/main.cpp
@@ -2,7 +2,23 @@
 
 int main()
 {
-	std::cout << "Hello, Asset Manager!" << std::endl;
+	CMP_InitFramework();
+
+	CMP_MipSet mipSet{};
+	auto error = CMP_LoadTexture("../../assets/planet.png", &mipSet);
+
+	if (error == CMP_OK)
+	{
+		std::cout << "Texture loaded!\n" << "Size: " << mipSet.m_nWidth << "x" << mipSet.m_nHeight << std::endl;
+	}
+	else
+	{
+		std::cerr << "Failed to load texture" << std::endl;
+		return -1;
+	}
+
+	CMP_FreeMipSet(&mipSet);
+
 	return 0;
 }
 
