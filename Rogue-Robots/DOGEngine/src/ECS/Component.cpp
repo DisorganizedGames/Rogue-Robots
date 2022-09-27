90e889f723263192830b4a209a5f0f670a31a594
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-09-27 12:28:33 +0200

    Add RotateW and RotateL functions to TransformComponent
    Moved definitions to cpp file

--- Rogue-Robots/DOGEngine/src/ECS/Component.cpp
+++ Rogue-Robots/DOGEngine/src/ECS/Component.cpp
@@ -1,4 +1,8 @@
 #include "Component.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
 namespace DOG
 {
 	static u32 componentID{ 0u };
@@ -8,4 +12,101 @@ namespace DOG
 		return componentID++;
 	}
 
+
+
+	// Math functions for the TransformComponent
+
+	TransformComponent::TransformComponent(const Vector3& position, const Vector3& rotation, const Vector3& scale) noexcept
+	{
+		auto t = Matrix::CreateTranslation(position);
+		auto r = Matrix::CreateFromYawPitchRoll(rotation);
+		auto s = Matrix::CreateScale(scale);
+		worldMatrix = s * r * t;
+	}
+
+	TransformComponent& TransformComponent::SetPosition(const Vector3& position) noexcept
+	{
+		worldMatrix.Translation(position);
+		return *this;
+	}
+
+	TransformComponent& TransformComponent::SetRotation(const Vector3& rotation) noexcept
+	{
+		XMVECTOR scale, rotationQuat, translation;
+		XMMatrixDecompose(&scale, &rotationQuat, &translation, worldMatrix);
+		worldMatrix = XMMatrixScalingFromVector(scale) *
+			XMMatrixRotationRollPitchYawFromVector(rotation) *
+			XMMatrixTranslationFromVector(translation);
+		return *this;
+	}
+
+	TransformComponent& TransformComponent::SetScale(const Vector3& scale) noexcept
+	{
+		XMVECTOR xmScale, rotationQuat, translation;
+		XMMatrixDecompose(&xmScale, &rotationQuat, &translation, worldMatrix);
+		worldMatrix = XMMatrixScalingFromVector(scale) *
+			XMMatrixRotationQuaternion(rotationQuat) *
+			XMMatrixTranslationFromVector(translation);
+		return *this;
+	}
+
+	Vector3 TransformComponent::GetPosition() const noexcept
+	{
+		return { worldMatrix(3, 0) , worldMatrix(3, 1), worldMatrix(3, 2) };
+	}
+
+	Matrix TransformComponent::GetRotation() const noexcept
+	{
+		XMVECTOR xmScale, rotationQuat, translation;
+		XMMatrixDecompose(&xmScale, &rotationQuat, &translation, worldMatrix);
+		return XMMatrixRotationQuaternion(rotationQuat);
+	}
+
+	TransformComponent& TransformComponent::RotateW(const Vector3& rotation) noexcept
+	{
+		XMVECTOR scaleVec, rotationQuat, translationVec;
+		XMMatrixDecompose(&scaleVec, &rotationQuat, &translationVec, worldMatrix);
+		XMMATRIX r = XMMatrixRotationQuaternion(rotationQuat);
+		XMMATRIX s = XMMatrixScalingFromVector(scaleVec);
+		XMMATRIX t = XMMatrixTranslationFromVector(translationVec);
+		XMMATRIX deltaR = XMMatrixRotationRollPitchYawFromVector(rotation);
+		worldMatrix = s * r * deltaR * t;
+		return *this;
+	}
+
+	TransformComponent& TransformComponent::RotateW(const Matrix& rotation) noexcept
+	{
+		XMVECTOR scaleVec, rotationQuat, translationVec;
+		XMMatrixDecompose(&scaleVec, &rotationQuat, &translationVec, worldMatrix);
+		XMMATRIX r = XMMatrixRotationQuaternion(rotationQuat);
+		XMMATRIX s = XMMatrixScalingFromVector(scaleVec);
+		XMMATRIX t = XMMatrixTranslationFromVector(translationVec);
+		XMMATRIX deltaR = rotation;
+		worldMatrix = s * r * deltaR * t;
+		return *this;
+	}
+
+	TransformComponent& TransformComponent::RotateL(const Vector3& rotation) noexcept
+	{
+		XMVECTOR scaleVec, rotationQuat, translationVec;
+		XMMatrixDecompose(&scaleVec, &rotationQuat, &translationVec, worldMatrix);
+		XMMATRIX r = XMMatrixRotationQuaternion(rotationQuat);
+		XMMATRIX s = XMMatrixScalingFromVector(scaleVec);
+		XMMATRIX t = XMMatrixTranslationFromVector(translationVec);
+		XMMATRIX deltaR = XMMatrixRotationRollPitchYawFromVector(rotation);
+		worldMatrix = s * deltaR * r * t;
+		return *this;
+	}
+
+	TransformComponent& TransformComponent::RotateL(const Matrix& rotation) noexcept
+	{
+		XMVECTOR scaleVec, rotationQuat, translationVec;
+		XMMatrixDecompose(&scaleVec, &rotationQuat, &translationVec, worldMatrix);
+		XMMATRIX r = XMMatrixRotationQuaternion(rotationQuat);
+		XMMATRIX s = XMMatrixScalingFromVector(scaleVec);
+		XMMATRIX t = XMMatrixTranslationFromVector(translationVec);
+		XMMATRIX deltaR = rotation;
+		worldMatrix = s * deltaR * r * t;
+		return *this;
+	}
 }
