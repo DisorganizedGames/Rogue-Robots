473001a3bc15e19940d1dc27922c9f668fdc1b88
Hostman68 <oscarmilstein48@gmail.com>
2022-10-07 15:03:33 +0200

    rebase

--- Rogue-Robots/DOGEngine/src/Graphics/Rendering/Renderer.cpp
+++ Rogue-Robots/DOGEngine/src/Graphics/Rendering/Renderer.cpp
@@ -520,6 +520,9 @@ namespace DOG::gfx
 			ZoneNamedN(RGExecuteScope, "RG Execution", true);
 			rg.Execute();
 		}
+		m_ui->m_d2d->BeginFrame();
+		m_ui->DrawUI();
+		m_ui->m_d2d->EndFrame();
 
 	}
 
