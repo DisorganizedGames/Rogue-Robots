6c0ed8e247e26b9c8aa814dbcc23914a0de8e10d
Nadhif Ginola <nfginola@gmail.com>
2022-10-18 17:40:43 +0200

    add tagging functionality to finished state

--- Rogue-Robots/Assets/LuaScripts/LuaStartUp.lua
+++ Rogue-Robots/Assets/LuaScripts/LuaStartUp.lua
@@ -1,36 +1,60 @@
 package.path = "Assets/LuaScripts/?.lua;" .. package.path
 
-local GameStateManager = {}
+local FinishedStateManager = {}
 
-GameStateManager["GroupIDs"] = 0;
-GameStateManager["WinGroups"] = {}
-GameStateManager["LoseGroups"] = {}
+FinishedStateManager["GroupIDs"] = 0;
+FinishedStateManager["FinishGroups"] = {}
+FinishedStateManager["Tag"] = {}
 
-function GameStateManager:CreateWinConditionGroup()
-	local i = GameStateManager["GroupIDs"]
-	GameStateManager["WinGroups"][i] = {}
-	GameStateManager["GroupIDs"] = GameStateManager["GroupIDs"] + 1
+
+function FinishedStateManager:CreateFinishGroup(tag)
+	local i = FinishedStateManager["GroupIDs"]
+	FinishedStateManager["Tag"][i] = tag or "DefaultTag"
+	FinishedStateManager["FinishGroups"][i] = {}
+	FinishedStateManager["GroupIDs"] = FinishedStateManager["GroupIDs"] + 1
 	return i
 end
 
-function GameStateManager:AddWinCondition(group, evalFunc)
-	print("XDD\n")
-	print(group)
-	table.insert(GameStateManager["WinGroups"][group], evalFunc)
+function FinishedStateManager:AddFinishCondition(group, evalFunc)
+	table.insert(FinishedStateManager["FinishGroups"][group], evalFunc)
+end
+
+
+
+
+function FinishedStateManager:GroupFinished(group)
+	local finished = true
+	for _, evalFunc in pairs(FinishedStateManager["FinishGroups"][group]) do
+		finished = finished and evalFunc()
+	end
+	return finished
+end
+
+function FinishedStateManager:AllGroupsFinished(tag)
+	local finished = true
+	local tagIn = tag or "DefaultTag"
+	local count = 0
+	for groupID, _ in pairs(FinishedStateManager["FinishGroups"]) do
+		local gtag = FinishedStateManager["Tag"][groupID]
+		if gtag == tagIn then
+			finished = finished and FinishedStateManager:GroupFinished(groupID)
+			count = count + 1
+		end
+	end
+	if count == 0 then
+		assert(false, "No finished groups with the tag: '" .. tag .. "'")
+	end
+	return finished
 end
 
 
 
 
 
-local a = GameStateManager.CreateWinConditionGroup()
-local b = GameStateManager.CreateWinConditionGroup()
 
 
 enemiesLeft = 0
 tokensGot = 5
-
-
 
 function EvalEnemiesWin()
 	print("Eval Enemies")
@@ -45,34 +69,30 @@ end
 local win1 = EvalEnemiesWin
 local win2 = EvalTokens
 
-GameStateManager:AddWinCondition(a, win1)
-GameStateManager:AddWinCondition(a, win2)
+
+local a = FinishedStateManager:CreateFinishGroup()
+
+FinishedStateManager:AddFinishCondition(a, win1)
+FinishedStateManager:AddFinishCondition(a, win2)
 
 
-p1Health = 19
+p1Health = 20
 p2Health = 20
 p3Health = 70
 p4Health = 21
 
+minHealth = 20
 function EvalHealth()
-	return p1Health >= 20 and p2Health >= 20 and p3Health >= 20 and p4Health >= 20
+	return p1Health >= minHealth and p2Health >= minHealth and p3Health >= minHealth and p4Health >= minHealth
 end
 
 local win3 = EvalHealth
 
-GameStateManager:AddWinCondition(b, win3)
+local b = FinishedStateManager:CreateFinishGroup()
+FinishedStateManager:AddFinishCondition(b, win3)
 
-local won = true
-for _, v in pairs(GameStateManager["WinGroups"]) do
 
-	-- Group
-	local groupWon = true
-	for _, evalFunc in pairs(v) do
-		groupWon = groupWon and evalFunc()
-	end
-	won = won and groupWon
-end
+print("Has won? ", FinishedStateManager:AllGroupsFinished())
 
-print("Has won? ", won)
 
 
