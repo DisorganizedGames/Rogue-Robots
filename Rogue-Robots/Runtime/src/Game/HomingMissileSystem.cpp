f8b8b4d02837dc13c7e0fcf121d15d2c4c591a18
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-10-26 20:11:37 +0200

    If missile collides with agent, kill the agent

--- Rogue-Robots/Runtime/src/Game/HomingMissileSystem.cpp
+++ Rogue-Robots/Runtime/src/Game/HomingMissileSystem.cpp
@@ -1,5 +1,5 @@
 #include "HomingMissileSystem.h"
-
+#include "AgentManager\AgentComponents.h"
 using namespace DOG;
 using namespace DirectX::SimpleMath;
 
@@ -17,6 +17,13 @@ void HomingMissileImpacteSystem::OnUpdate(entity e, HomingMissileComponent& miss
 {
 	if (missile.launched && collision.entitiesCount > 0)
 	{
+		for (u32 i = 0; i < collision.entitiesCount; i++)
+		{
+			if (EntityManager::Get().HasComponent<AgentHPComponent>(collision.entities[i]))
+			{
+				EntityManager::Get().GetComponent<AgentHPComponent>(collision.entities[i]).hp = 0;
+			}
+		}
 		EntityManager::Get().DeferredEntityDestruction(e);
 	}
 }
