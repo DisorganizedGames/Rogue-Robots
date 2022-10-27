2399c99e11a1c4a9853335c3a4a05e45491f2504
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-10-27 20:20:03 +0200

    Add targeting system fort missiles, they do splash dmg

--- Rogue-Robots/Runtime/src/Game/HomingMissileSystem.cpp
+++ Rogue-Robots/Runtime/src/Game/HomingMissileSystem.cpp
@@ -13,18 +13,39 @@ using namespace DirectX::SimpleMath;
 
 
 
-void HomingMissileImpacteSystem::OnUpdate(entity e, HomingMissileComponent& missile, DOG::HasEnteredCollisionComponent& collision)
+void HomingMissileImpacteSystem::OnUpdate(entity e, HomingMissileComponent& missile, DOG::HasEnteredCollisionComponent& collision, DOG::TransformComponent& transform)
 {
 	if (missile.launched && collision.entitiesCount > 0)
 	{
-		for (u32 i = 0; i < collision.entitiesCount; i++)
-		{
-			if (EntityManager::Get().HasComponent<AgentHPComponent>(collision.entities[i]))
+		EntityManager::Get().Collect<AgentHitComponent, DOG::TransformComponent>().Do([&](AgentHitComponent& agentHit, DOG::TransformComponent& tr)
 			{
-				EntityManager::Get().GetComponent<AgentHPComponent>(collision.entities[i]).hp = 0;
-			}
-		}
-		EntityManager::Get().AddComponent<ExplosionEffectComponent>(e, 5.0f);
+				float distSquared = Vector3::DistanceSquared(transform.GetPosition(), tr.GetPosition());
+				if (distSquared < missile.explosionRadius * missile.explosionRadius)
+				{
+					agentHit.HitBy({e, missile.dmg / (1.0f + distSquared) , missile.playerNetworkID });
+				}
+			});
+
+		EntityManager::Get().AddComponent<ExplosionEffectComponent>(e, 0.8f * missile.explosionRadius);
 		EntityManager::Get().DeferredEntityDestruction(e);
+	}
+}
+
+void HomingMissileTargetingSystem::OnUpdate(HomingMissileComponent& missile, DOG::TransformComponent& transform)
+{
+	float minDistSquared = 1000000;
+	entity target = NULL_ENTITY;
+	if (missile.homingTarget == NULL_ENTITY)
+	{
+		EntityManager::Get().Collect<AgentHPComponent, DOG::TransformComponent>().Do([&](entity e, AgentHPComponent&, DOG::TransformComponent& tr)
+			{
+				float distSquared = Vector3::DistanceSquared(tr.GetPosition(), transform.GetPosition());
+				if (distSquared < minDistSquared)
+				{
+					minDistSquared = distSquared;
+					target = e;
+				}
+			});
+		missile.homingTarget = target;
 	}
 }
