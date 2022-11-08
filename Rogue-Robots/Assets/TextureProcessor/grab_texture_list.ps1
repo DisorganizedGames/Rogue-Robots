c9fab11aad928d3f3d39e9e0934984ab238d179b
Nadhif Ginola <nfginola@gmail.com>
2022-11-08 19:24:29 +0100

    create dds only if it hasn't been done

--- Rogue-Robots/Assets/TextureProcessor/grab_texture_list.ps1
+++ Rogue-Robots/Assets/TextureProcessor/grab_texture_list.ps1
@@ -8,9 +8,18 @@ foreach($line in Get-Content .\pngs_to_process.txt) {
         # Remove whitspace
         $line = $line -replace '\s',''
 
+        $outputLine = $line
+        $outputLine = [IO.Path]::ChangeExtension($outputLine, "dds")
+
         $ext = [IO.Path]::GetExtension($line)
         if ($ext -eq ".png")
         {
+            $exists = Test-Path -Path $outputLine -PathType Leaf
+            if ($exists -eq $true)
+            {
+                continue
+            }
+
             $dir = [IO.Path]::GetDirectoryName($line) 
             &.\texconv -pow2 -f BC7_UNORM_SRGB $line -srgb -y -m 1 -o $dir
         }
@@ -24,10 +33,23 @@ foreach($line in Get-Content .\jpgs_to_process.txt) {
         $line = $line -replace '\s',''
         $ext = [IO.Path]::GetExtension($line)
 
+        
+        $outputLine = $line
+        $outputLine = [IO.Path]::ChangeExtension($outputLine, "dds")
+
         if ($ext -eq ".jpg")
         {
+            $exists = Test-Path -Path $outputLine -PathType Leaf
+            if ($exists -eq $true)
+            {
+                continue
+            }
+
             $dir = [IO.Path]::GetDirectoryName($line) 
             &.\texconv -pow2 -f BC7_UNORM_SRGB $line -srgb -y -m 1 -o $dir
         }
     }
 }
+
+rm pngs_to_process.txt
+rm jpgs_to_process.txt
