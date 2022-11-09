fcd5ab8f7b185347c2fa64fd61260b9739accc7c
AxLundberg <axel__lundberg@hotmail.com>
2022-11-09 13:19:16 +0100

    minot fixes
    some dirty fixes for scaling model.
    
    Correctly scaled model resides on my office PC

--- Rogue-Robots/DOGEngine/src/Core/AnimationManager.h
+++ Rogue-Robots/DOGEngine/src/Core/AnimationManager.h
@@ -6,7 +6,6 @@
 
 #include "Types/AssetTypes.h"
 #include "DOGEngineTypes.h"
-#include "Animator.h"
 #include "RigAnimator.h"
 
 namespace DOG
@@ -19,7 +18,6 @@ namespace DOG
 		static constexpr u8 N_KEYS = 3;
 		static constexpr u8 MAX_CLIPS = 10;
 	private:
-		using ClipData = DOG::Animator::ClipRigData;
 		enum class KeyType
 		{
 			Scale = 0,
@@ -36,46 +34,35 @@ namespace DOG
 		void Test(f32 dt);
 		void UpdateAnimationComponent(const std::vector<DOG::AnimationData>& animations, DOG::AnimationComponent& ac, const f32 dt) const;
 		void UpdateSkeleton(DOG::RigAnimator& animator, const u32 offset);
-		void UpdateSkeleton(const DOG::ImportedRig& rig, const DOG::Animator& animator);
+		void SetPlayerBaseStates();
 
 		DirectX::FXMVECTOR GetKeyValue(const std::vector<DOG::AnimationKey>& keys, const KeyType& component, f32 tick);
-		DirectX::FXMVECTOR ExtractScaling(const i32 nodeID, const DOG::ImportedRig& rig, const DOG::Animator& ac);
-		DirectX::FXMVECTOR ExtractRotation(const i32 nodeID, const DOG::ImportedRig& rig, const DOG::Animator& ac);
-		DirectX::FXMVECTOR ExtractWeightedAvgRotation(const i32 nodeID, const DOG::ImportedRig& rig, const DOG::Animator& ac);
-		DirectX::FXMVECTOR ExtractRootTranslation(const i32 nodeID, const DOG::ImportedRig& rig, const DOG::Animator& ac);
 
-		void CalculateSRT(const std::vector<AnimationData>& anims, const Animator& ac, const u8 rigID);
 		// Gets the S/R/T keyframe data from active animation clips in animation component
 		void ExtractClipNodeInfluences(const ClipData* pcData, const std::vector<AnimationData>& anims, const KeyType key, const u8 nClips, const u8 rigID, const u8 group);
 
 		// RIG ANIMATOR
 		void CalculateSRT(RigAnimator& ac, const u8 rigID);
 		void ExtractClipNodeInfluences(RigAnimator& animator, const KeyType key, const u32 group, const u32 rigID);
-
-		i8 GetNextAnimatorID()
-		{
-			static i8 id = 0;
-			return id++;
-		}
 	private:
 		std::vector<ImportedRig*> m_rigs;
-		std::array<Animator, 4> m_playerAnimators;
 		std::array<RigAnimator, 4> m_playerRigAnimators;
-		std::array<DirectX::XMVECTOR, MAX_CLIPS * NodeCount(MIXAMO_RIG_ID)> m_partialSRT{DirectX::XMVECTOR{}};
-		std::array<DirectX::XMVECTOR, MAX_CLIPS * NodeCount(MIXAMO_RIG_ID)> m_fullbodySRT{DirectX::XMVECTOR{} };
+		std::array<DirectX::XMVECTOR, MAX_CLIPS* NodeCount(MIXAMO_RIG_ID)> m_partialSRT{ DirectX::XMVECTOR{} };
+		std::array<DirectX::XMVECTOR, MAX_CLIPS* NodeCount(MIXAMO_RIG_ID)> m_fullbodySRT{ DirectX::XMVECTOR{} };
 	private:
 		bool m_bonesLoaded = false;
+		bool m_playerAnimatorsLoaded = false;
 		static constexpr i32 ROOT_NODE = 0;
 		static constexpr i32 ROOT_JOINT = 2;
 		static constexpr DirectX::XMFLOAT3 m_baseScale = { 1.f, 1.f, 1.f };
 		static constexpr DirectX::XMFLOAT4 m_baseRotation = { 0.f, 0.f, 0.f, 0.f };
 		static constexpr DirectX::XMFLOAT3 m_baseTranslation = { 0.f, 0.f, 0.f };
-		
+
 		// IMGUI RELATED
 	private:
 		RigAnimator mRigAnimator;
 		f32 m_imguiGroupWeightA = 0.0f;
-		bool m_imguiApplyRootTranslation = false;
+		bool m_imguiApplyRootTranslation = true;
 		DirectX::FXMMATRIX ImguiTransform(i32 joint);
 		std::vector<DirectX::XMFLOAT3> m_imguiSca;
 		std::vector<DirectX::XMFLOAT3> m_imguiRot;
--- Rogue-Robots/DOGEngine/src/Core/CoreUtils.h
+++ Rogue-Robots/DOGEngine/src/Core/CoreUtils.h
@@ -15,7 +15,7 @@ namespace DOG
 
 		bool ssao{ true };
 #if defined _DEBUG
-		bool lit{ false };
+		bool lit{ true };
 		bool shadowMapping{ false };
 #else
 		bool lit{ true };
