d842f27f3d7822e47d85022fe77c0764a32cf5e8
Gunnar Cerne <bjorngunnar99@gmail.com>
2022-11-24 20:26:46 +0100

    Add particle emitter when turret hits something

--- Rogue-Robots/Runtime/src/Game/GameLayer.cpp
+++ Rogue-Robots/Runtime/src/Game/GameLayer.cpp
@@ -41,6 +41,7 @@ GameLayer::GameLayer() noexcept
 	m_entityManager.RegisterSystem(std::make_unique<TurretTargetingSystem>());
 	m_entityManager.RegisterSystem(std::make_unique<TurretShootingSystem>());
 	m_entityManager.RegisterSystem(std::make_unique<TurretProjectileSystem>());
+	m_entityManager.RegisterSystem(std::make_unique<TurretProjectileHitSystem>());
 	m_entityManager.RegisterSystem(std::make_unique<LaserShootSystem>());
 	m_entityManager.RegisterSystem(std::make_unique<LaserBeamSystem>());
 	m_entityManager.RegisterSystem(std::make_unique<LaserBeamVFXSystem>());
