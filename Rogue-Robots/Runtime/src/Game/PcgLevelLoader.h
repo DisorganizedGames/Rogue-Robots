c7a3571461510ad80d13b013b61a17d778567a76
Emil Högstedt <emil.hogstedt@hotmail.com>
2022-12-08 21:06:22 +0100

    Removed broken level and renamed all levels

--- Rogue-Robots/Runtime/src/Game/PcgLevelLoader.h
+++ Rogue-Robots/Runtime/src/Game/PcgLevelLoader.h
@@ -8,18 +8,17 @@ namespace pcgLevelNames
 {
 	constexpr const char* oldDefault = "..\\Offline-Tools\\PCG\\showOff_generatedLevel.txt";
 	constexpr const char* tunnels = "..\\Offline-Tools\\PCG\\Tunnels_generatedLevel.txt";
-	constexpr const uint32_t nrLevels = 10;
+	constexpr const uint32_t nrLevels = 9;
 	constexpr const char pcgLevels[nrLevels][128] = {
-		"WorkingLevel1.txt",
-		"WorkingLevel2.txt",
-		"WorkingLevel3.txt",
-		"WorkingLevel4.txt",
-		"WorkingLevel5.txt",
-		"WorkingLevel6.txt",
-		"WorkingLevel7.txt",
-		"WorkingLevel8.txt",
-		"WorkingLevel9.txt",
-		"WorkingLevel10.txt"
+		"Cave.txt",
+		"Bridge.txt",
+		"Tunnels.txt",
+		"Bamboozle.txt",
+		"Crazy.txt",
+		"Omega.txt",
+		"Infinity.txt",
+		"Abyss.txt",
+		"Impossible.txt"
 	};
 }
 std::vector<DOG::entity> LoadLevel(std::string file); //Loads a PCG generated level.
