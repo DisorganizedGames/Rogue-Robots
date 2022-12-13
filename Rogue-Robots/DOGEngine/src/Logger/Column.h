f7cafe31c146a46d56be6101da5974316da1bad5
Ove Ødegård <ove.odegard@gmail.com>
2022-12-13 15:49:11 +0100

    fixed requested changes

--- Rogue-Robots/DOGEngine/src/Logger/Column.h
+++ Rogue-Robots/DOGEngine/src/Logger/Column.h
@@ -7,16 +7,17 @@ namespace DOG
 	public:
 		std::string& operator[](size_t row) { return m_values[row]; };
 
-		void Add(std::string	value) { m_values.push_back(value); }
-		void Add(u64			value) { m_values.push_back(std::to_string(value)); }
-		void Add(i64			value) { m_values.push_back(std::to_string(value)); }
-		void Add(u32			value) { m_values.push_back(std::to_string(value)); }
-		void Add(i32			value) { m_values.push_back(std::to_string(value)); }
-		void Add(u8				value) { m_values.push_back(std::to_string(static_cast<u32>(value))); }
-		void Add(i8				value) { m_values.push_back(std::to_string(static_cast<i32>(value))); }
-		void Add(f64			value) { m_values.push_back(std::to_string(value)); }
-		void Add(f32			value) { m_values.push_back(std::to_string(value)); }
-		void Add(bool			value) { m_values.push_back(std::to_string(value)); }
+		using str = std::string;
+		void Add(str value) { m_values.push_back(value); }
+		void Add(u64 value) { m_values.push_back(std::to_string(value)); }
+		void Add(i64 value) { m_values.push_back(std::to_string(value)); }
+		void Add(u32 value) { m_values.push_back(std::to_string(value)); }
+		void Add(i32 value) { m_values.push_back(std::to_string(value)); }
+		void Add(u8 value) { m_values.push_back(std::to_string(static_cast<u32>(value))); }
+		void Add(i8 value) { m_values.push_back(std::to_string(static_cast<i32>(value))); }
+		void Add(f64 value) { m_values.push_back(std::to_string(value)); }
+		void Add(f32 value) { m_values.push_back(std::to_string(value)); }
+		void Add(bool value) { m_values.push_back(std::to_string(value)); }
 		
 		size_t size() { return m_values.size(); }
 
